bcdccc4d74e070cc160d8867fb63ea0b72a66179
Vikank <62172501+Vikank@users.noreply.github.com>
2025-09-05 15:31:29 +0530

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const responseMiddleware = require("./utils/responseMiddleware");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(helmet());
 
